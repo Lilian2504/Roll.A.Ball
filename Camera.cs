ae23863e524a2f468241b17e141a14c3c2bbded4
Lilian2504 <61809430+Lilian2504@users.noreply.github.com>
2020-03-05 22:18:37 -0300

    Add files via upload

---
+++ Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+	public GameObject player; 
+	private Vector3 deslocamento;
+    // Start is called before the first frame update
+    void Start()
+    {
+		deslocamento=transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position=player.transform.position+deslocamento;
+    }
+}
---
+++ MovimentoPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovimentoPlayer : MonoBehaviour
+{
+   
+	private Rigidbody rb;
+	public float velocidade;
+	private int count;
+	public Text winText;
+
+	void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+		count=0;
+		SetCountText();
+		winText.text="";
+    }
+
+  
+    void Update()
+    {
+		
+		float movimentoHorizontal = Input.GetAxis("Horizontal");
+		float movimentoVertical = Input.GetAxis("Vertical");
+
+		Vector3 movimento = new Vector3(movimentoHorizontal, 0.0f, movimentoVertical);
+		rb.AddForce(movimento * velocidade);
+    }
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag=="PickUp"){
+			other.gameObject.SetActive(false);
+			count=count+1;
+			SetCountText();
+		}
+	}
+	public Text countText;
+
+	void SetCountText(){
+		countText.text="Count: "+count.ToString();
+		if (count>=12)
+			winText.text="YOU WIN!!";
+	}
+}
